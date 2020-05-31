85838b875f082f63598419a7768df76fd54c0a73
Bob8552 <63597378+bob8552@users.noreply.github.com>
2020-05-31 17:46:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('view-engine', 'ejs')
+app.set('view-engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "views")));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.set({'Content-Type': 'text/html'});
 
